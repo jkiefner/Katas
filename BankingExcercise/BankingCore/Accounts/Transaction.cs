76f7bdd2c5b2dfa583a6862ba9b02b26241e73a7
Jamie Kiefner <jkiefner@gmail.com>
2012-03-20 19:12:47 -0400

    - Added Exercise outline and Transaction class.

---
+++ BankingExcercise/BankingCore/Accounts/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankingCore.Accounts
+{
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public Guid TransactionId { get; set; }
+        public int AccountNumber { get; set; }
+        public decimal TransactionAmount { get; set; }
+        public decimal PriorBalance { get; set; }
+        public decimal Balance { get; set; }
+        public string Description { get; set; }
+    }
+}
