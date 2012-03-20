c404440093980b373306dc0f7ef8678da0fb4514
Jamie Kiefner <jkiefner@gmail.com>
2012-03-20 12:54:43 -0400

    - Added AccountRepository and TestFixture.

---
+++ BankingExcercise/BankingCore/Accounts/AccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BankingCore.Users;
+
+namespace BankingCore.Accounts
+{
+    public class AccountRepository
+    {
+
+    }
+}
---
+++ BankingExcercise/BankingCoreTests/Accounts/AccountRepositoryTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BankingCore.Users;
+using NUnit.Framework;
+
+namespace BankingCoreTests.Accounts
+{
+    [TestFixture]
+    public class AccountRepositoryTests
+    {
+        
+    }
+}
